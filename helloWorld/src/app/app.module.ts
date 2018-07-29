c6583f7bd6221205badce76e440d5bf04b65fe25
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-07-29 13:20:19 +0530

    sidemenu working; need to add list contents

--- helloWorld/src/app/app.module.ts
+++ helloWorld/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ListPage } from '../[pages/list/list]';
+
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ListPage
   ],
   imports: [
     BrowserModule,
@@ -20,6 +23,7 @@ import { HomePage } from '../pages/home/home';
   entryComponents: [
     MyApp,
     HomePage
+    ListPage
   ],
   providers: [
     StatusBar,
---
+++ helloWorld/src/pages/list/list.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+export class ListPage {
+  selectedItem: any;
+  icons: string[];
+  items: Array<{title: string, note: string. icon: string}>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams){
+    // Upon navigating to this page, we should have a nav param item available
+    this.selectedItem = navParams.get('item');
+
+    // sample glyph content
+    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    'american-football', 'boat', 'bluetooth', 'build'];
+
+    this.items = [];
+    for (let i = 1; i < 11; i++){
+      this.items.push({
+        title: 'Item ' + 1,
+        note: 'This is item #' + i,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+    }
+  }
+
+  itemTapped(event, item){
+    // On item being tapped push litem content from navCtrl
+    this.navCtrl.push(ListPage,{
+      item: item
+    });
+  }
+}
