545ca616ab316ecab3b9f8398189c723c8aa74cd
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-07-29 12:32:40 +0530

    no list found

--- helloWorld/src/app/app.component.ts
+++ helloWorld/src/app/app.component.ts
@@ -1,22 +1,41 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list'
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+  pages: Array<{title:string, component: any}>;
+
+  constructor(platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    this.intitializeApp();
+    // used for ngFor and navigation
+
+    this.pages = [
+        { title: 'Home', component: HomePage },
+        { title: 'List', component: ListPage }
+    ];
+
+  }
+
+  initializeApp(){
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
+  openPage(page) {
+      // show no back button
+      this.nav.setRoot(page.component);
+  }
 }
-
